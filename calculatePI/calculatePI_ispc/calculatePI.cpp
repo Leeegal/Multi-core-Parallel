b10266544af04d862ae20f92b2b0f113f1e24e07
Liangtj <liangtjsky@gmail.com>
2018-06-04 23:20:38 +0800

    添加了ispc的计算PI

---
+++ calculatePI/calculatePI_ispc/calculatePI.cpp
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <sys/time.h>
+#include <iostream>
+#include <chrono>
+
+#include "calculatePI_ispc.h"
+using namespace ispc;
+
+#define PALNUM 100
+
+int main() {
+    auto start = std::chrono::system_clock::now();
+
+    double sum = calPi(PALNUM);
+
+    
+    std::cout << "PI: " << sum << std::endl;
+    auto end = std::chrono::system_clock::now();
+    std::chrono::duration<double> diff = end-start;
+    std::cout << "time: " << diff.count() << std::endl;
+
+    return 0;
+
+}
